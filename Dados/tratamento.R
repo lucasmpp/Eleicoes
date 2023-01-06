9e102aa7da00175ed377a4234ec3dfd936626b13
Gabriel Lopes <gabrielopecs@gmail.com>
2023-01-06 15:59:01 -0300

    Arrumando o Label

--- Dados/tratamento.R
+++ Dados/tratamento.R
@@ -75,18 +75,25 @@ for(linha in rank_cor$rank){
 ## Funções
 
 label.function <- function(df,coluna,diff=FALSE){
+  if(diff){
+    df$VOTOS <- paste0(ifelse(df$VOTOS>0,"+",""),df$VOTOS)
+  }else{
+    df <- df%>%
+      group_by_at(c(1,3)) %>%
+      mutate(`VOTOS` = `VOTOS`/sum(`VOTOS`))
+    df$VOTOS <- round(df$VOTOS*100,2)
+  }
+  
   df.1 <- df %>%
     group_by_at(match(c('NR_TURNO',coluna),names(df)))%>% # Pela coluna
     arrange(desc(VOTOS))%>%
-    summarize(labels = paste(NM_VOTAVEL,": ",
-                              ifelse(diff,
-                                    ifelse(VOTOS>0,"+",""),""),
-                              VOTOS,"%", collapse = '<br/>'))
-  
+    summarize(labels = paste(NM_VOTAVEL,": ",VOTOS,"%", collapse = '<br/>'))
+  df.1$labels
   return(df.1)
 }
 
 tratamento <- function(dados_base, geografia, coluna, diff = FALSE){
+  print(diff)
   df <- dados_base
   myCols <- c('NR_TURNO', 'NM_VOTAVEL', coluna,'VOTOS')
   df <- df %>% 
@@ -119,12 +126,17 @@ tratamento <- function(dados_base, geografia, coluna, diff = FALSE){
   if(diff==FALSE){
     df$TOTAL <- rowSums( df[,3:ncol(df)] )
     df[,-c(1,2,ncol(df))] <- df[,-c(1,2,ncol(df))]/df$TOTAL
+    print("total")
   }
+  
   df$Ganhador = temp$Ganhador
   df <- left_join(df,tabela.labels)
   df <- left_join(df, geografia, by = coluna)
   df <- na.omit(df)
-  df$labels <-  paste0(paste(df$name_state,", ", df$name_region,"<br/>"),df$labels )%>% lapply(htmltools::HTML)
+  df$labels <-  paste0(paste(
+    ifelse(is.na(df$name_state),"",
+           paste0(df$name_state,", ")),
+           df$name_region,"<br/>"),df$labels) %>% lapply(htmltools::HTML)
   
   #Paleta.cores(sub("\\$.*", "", deparse(substitute(df$Ganhador))), df$Ganhador)
   
@@ -161,7 +173,7 @@ mapa <- function(df, turno, diff=FALSE){
       smoothFactor = 0.5,
       fillColor = ~ cor,
       fillOpacity = 1,
-      label = labels,
+      label = df$labels,
       labelOptions =  labelOptions(
         style = list("font-weigth"= "normal",
                      padding = "3px 8px"),
@@ -171,35 +183,31 @@ mapa <- function(df, turno, diff=FALSE){
 }
 
 ###
-#TESTES 
-dados_mun <- tratamento(dados_base,geo_mun,'CD_MUNICIPIO', diff = TRUE)
-dados_mun$cor <- Paleta.cores('dados_mun',dados_mun$Ganhador)
-mapa(dados_mun,1,diff = TRUE)
-
-
 # Banco de dados
 
 dados_mun <- tratamento(dados_base,geo_mun,'CD_MUNICIPIO')
-dados_est <- tratamento(dados_base,geo_est,'SG_UF')
-dados_reg <- tratamento(dados_base,geo_reg,'name_region')
-dados_reg_diff <- tratamento(dados_base,geo_reg,'name_region', diff=TRUE)
-
-remove(geo_mun,geo_est,geo_reg)
-
-# Definindo a paleta de cores
-
 dados_mun$cor <- Paleta.cores('dados_mun',dados_mun$Ganhador)
-dados_est$cor <- Paleta.cores('dados_est',dados_est$Ganhador)
-dados_reg$cor <- Paleta.cores('dados_reg',dados_reg$Ganhador)
-
-# save(dados_mun, file = "Dados/dados_mun.Rdata")
-# save(dados_est, file = "Dados/dados_est.Rdata")
-# save(dados_reg, file = "dados_reg.Rdata")
-
-mapa(dados_mun,1,diff = TRUE)
+mapa(dados_mun,1)
 mapa(dados_mun,2)
+
+
+dados_est <- tratamento(dados_base,geo_est,'SG_UF')
+dados_est$cor <- Paleta.cores('dados_est',dados_est$Ganhador)
 mapa(dados_est,1)
 mapa(dados_est,2)
+
+dados_reg <- tratamento(dados_base,geo_reg,'name_region')
+dados_reg$cor <- Paleta.cores('dados_reg',dados_reg$Ganhador)
 mapa(dados_reg,1)
-mapa(dados_reg,2,diff = TRUE)
+mapa(dados_reg_diff,2)
+
+dados_reg_diff <- tratamento(dados_base,geo_reg,'name_region', diff=TRUE)
+dados_reg_diff$cor <- Paleta.cores('dados_reg_diff',dados_reg_diff$Ganhador)
+mapa(dados_reg,1,diff = TRUE)
+mapa(dados_reg_diff,2,diff = TRUE)
+
+#remove(geo_mun,geo_est,geo_reg)
+
+
+
 
